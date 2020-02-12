d0f21c3ccb0d321c26e06668d01753170261fc64
Dahae Oh <dhekgo408@naver.com>
2020-02-12 16:12:26 +0900

    20200212 lecture

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+const userRouter = require('./userRouter');
+
+app.use('/user', userRouter);
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+
+    res.send('app.js');
+
+});
+
+app.get('/index', function(req, res) {
+
+    res.send('app.js');
+
+});
+
+app.get('*',function(req, res){
+    res.status(404).send('File not found');
+});
+app.listen(3000);
+
+
---
+++ userRouter.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const dbModule = require('./db-module');
+const sqlite3 = require('sqlite3').verbose(); 
+
+const router = express.Router();
+
+router.get('/list', function(req, res) {
+    dbModule.getAllUsers(function(rows) {
+        let view = require('./view/userList');
+        let html = view.userList(rows);
+        console.log(rows);
+        res.send(html);
+    });
+});
+
+router.get('/register', function(req, res) {
+    res.send('register');
+});
+
+router.get('/update/uid/:uid', function(req, res) {
+    res.send('update');
+    
+});
+
+router.get('/password/uid/:uid', function(req, res) {
+    res.send('password');
+});
+
+router.get('/delete/uid/:uid', function(req, res) {
+    res.send('delete');
+});
+
+module.exports = router;
